f7ddda74c69348ad7c5891c0851cae3b7be108f0
Felipe Jorge <felipe.jorge.2g@gmail.com>
2023-11-05 23:40:02 -0300

    Atualização no códgo. mundança para o metodo de ordenação B?ubbleSort. Mudanças  de integração dos metodos na classe main e criação da minha classe lista

---
+++ src/Lista.java
@@ -0,0 +1,169 @@
+
+public class Lista {
+	
+	
+	private static class Celula { Object item; Celula prox; }
+	private Celula primeiro, ultimo, pos;
+	// Operações
+	public Lista () { // Cria uma Lista vazia
+	this.primeiro = new Celula (); 
+	this.pos = this.primeiro; 
+	this.ultimo = this.primeiro; 
+	this.primeiro.prox = null;
+	}
+	
+	public Object pesquisa (Object chave) {
+	if (this.vazia () || chave == null) {
+		return null; }
+		Celula aux = this.primeiro;
+		while (aux.prox != null) {
+			if (aux.prox.item.equals(chave)) {
+				return aux.prox.item;
+			}
+		aux= aux.prox;
+		} return null;
+	}
+
+	public void insere (Object x) {
+		this.ultimo.prox = new Celula ();
+		this.ultimo = this.ultimo.prox;
+		this.ultimo.item = x; this.ultimo.prox = null;
+		}
+
+	public Object retira (Object chave) 
+		throws Exception {
+		if (this.vazia () || (chave == null)) {
+			throw new Exception 
+			("Erro: Lista vazia ou chave invalida");
+			}
+			
+		Celula aux =this.primeiro;
+		while (aux.prox!= null && !aux.prox.item.equals (chave)) 
+			aux=aux.prox;
+		if (aux.prox == null)
+			return null; 
+		Celula q = aux.prox;
+		Object item =q.item; 
+		aux.prox = q.prox;
+		if (aux.prox == null) 
+			this.ultimo = aux; 
+		return item;
+		}
+	
+	public Object retiraPrimeiro () throws Exception {
+		if (this.vazia ()) throw new Exception
+		("Erro: Lista vazia");
+		Celula aux = this.primeiro; 
+		Celula q = aux.prox;
+		Object item= q.item; aux.prox = q.prox;
+		if (aux.prox == null) this. ultimo = aux; 
+		return item;
+		}
+	
+	public Object retiraPorTitulo(String titulo) throws Exception {
+	    if (this.vazia() || (titulo == null || titulo.isEmpty())) {
+	        throw new Exception("Erro: Lista vazia ou título inválido");
+	    }
+
+	    Celula aux = this.primeiro;
+	    while (aux.prox != null) {
+	        Livro livro = (Livro) aux.prox.item;
+	        if (livro.getTitulo().equals(titulo)) {
+	            Celula q = aux.prox;
+	            Object item = q.item;
+	            aux.prox = q.prox;
+	            if (aux.prox == null) {
+	                this.ultimo = aux;
+	            }
+	            return item;
+	        }
+	        aux = aux.prox;
+	    }
+	    return null;
+	}
+
+	public Object retiraPorAutor(String autor) throws Exception {
+	    if (this.vazia() || (autor == null || autor.isEmpty())) {
+	        throw new Exception("Erro: Lista vazia ou autor inválido");
+	    }
+
+	    Celula aux = this.primeiro;
+	    while (aux.prox != null) {
+	        Livro livro = (Livro) aux.prox.item;
+	        if (livro.getAutor().equals(autor)) {
+	            Celula q = aux.prox;
+	            Object item = q.item;
+	            aux.prox = q.prox;
+	            if (aux.prox == null) {
+	                this.ultimo = aux;
+	            }
+	            return item;
+	        }
+	        aux = aux.prox;
+	    }
+	    return null;
+	}
+	
+	
+	public Object primeiro () {
+		this.pos = primeiro; return proximo (); 
+		}
+
+
+	public Object proximo () {
+		this.pos = this.pos.prox;
+		if (this.pos == null) return null;
+		else return this.pos.item;
+		}
+
+
+	public boolean vazia () {
+		return (this.primeiro == this.ultimo);
+		}
+	
+	public void imprime () {
+		Celula aux = this.primeiro.prox;
+		while (aux != null) {
+		System.out.println (aux.item.toString()); 
+		aux = aux.prox; 
+			} 
+		}
+
+	public void ordenaBubbleSort(String criterio) {
+	    if (vazia()) {
+	        return;
+	    }
+
+	    Celula i = primeiro;
+	    while (i != null) {
+	        Celula j = primeiro.prox;
+	        while (j.prox != null) {
+	            Livro livro1 = (Livro) j.item;
+	            Livro livro2 = (Livro) j.prox.item;
+
+	            // criterio de ordenaçã
+	            int comparacao;
+	            if (criterio.equals("titulo")) {
+	                comparacao = livro1.getTitulo().compareTo(livro2.getTitulo());
+	            } else if (criterio.equals("autor")) {
+	                comparacao = livro1.getAutor().compareTo(livro2.getAutor());
+	            } else {
+	                throw new IllegalArgumentException("Critério de ordenação inválido: " + criterio);
+	            }
+
+	            if (comparacao > 0) {
+	                Object temp = j.item;
+	                j.item = j.prox.item;
+	                j.prox.item = temp;
+	            }
+
+	            j = j.prox;
+	        }
+	        i = i.prox;
+	    }
+	}
+
+	
+	
+
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,112 +1,70 @@
-import java.util.List;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-
-
-
-
 
 public class Principal {
 	
-	
+	// Método para criar um novo livro
 	public static Livro criarLivro() {
 	    Scanner leitor = new Scanner(System.in);
-
 	    System.out.println("Digite o título do livro:");
 	    String titulo = leitor.nextLine();
-
 	    System.out.println("Digite o autor do livro:");
 	    String autor = leitor.nextLine();
-
 	    System.out.println("Digite o ISBN do livro:");
 	    String ISBN = leitor.nextLine();
-
 	    System.out.println("Digite a disponibilidade do livro:");
 	    String disponibilidade = leitor.nextLine();
-
 	    System.out.println("Digite o critério de ordenação do livro:");
 	    String criterioDeOrdenacao = leitor.nextLine();
-
 	    return new Livro(titulo, autor, ISBN, disponibilidade, criterioDeOrdenacao);
 	}
 
-	public static void adicionarLivro(List<Livro> livros) {
-	    Livro meuLivroUsuario = criarLivro();
-	    livros.add(meuLivroUsuario);
-	}
-
-    public static void removerLivroPorTitulo(String titulo, List<Livro> livros) {
-        for (int i = 0; i < livros.size(); i++) {
-            if (livros.get(i).getTitulo().equals(titulo)) {
-                livros.remove(i);
-                System.out.println("Livro removido com sucesso!");
-                return;
-            }
-        }
-        System.out.println("Livro não encontrado!");
-    }
-
-	
-    public static void main(String[] args) {
-        String csvFile = "C:\\Users\\Felipe Jorge\\eclipse-workspace\\SistemaDeGerenciamentoDeLivros\\CADASTRO-100-LIVROS.csv" ;
+    public static void main(String[] args) throws Exception {
+        String csvFile = "C:\\Users\\Felipe Jorge\\eclipse-workspace\\SistemaDeGerenciamentoDeLivros\\CADASTRO-100-LIVROS.csv";
         String line;
         String csvSplitBy = ",";
-        
-        
         Scanner leitor = new Scanner(System.in);
-        
-        // Criando uma lista para armazenar os livros
-        List<Livro> livros = new ArrayList<>();
+        Lista livros = new Lista();
 
-       
-        
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
+                String[] livro = line.split(csvSplitBy);
+                Livro meuLivro = new Livro(livro[0], livro[1], livro[2], livro[3], livro[4]);
+               
+                
+                Livro novoLivro = criarLivro();
+                livros.insere(novoLivro);
 
-        	String[] livro = line.split(csvSplitBy);
-
-            // Criando uma nova instância da classe Livro
-            Livro meuLivro = new Livro(livro[0], livro[1], livro[2], livro[3], livro[4]);
-
-            // o método adicionarLivro faz a criação do livro e da adição à lista.
-            //adicionarLivro(livros);
-
-            
-            // Removendo um livro pelo título
-           /* System.out.println("Digite o título do livro que deseja remover:");
-            String titulo=leitor.nextLine();
-            removerLivroPorTitulo(titulo, livros); 
-            */
-            // Imprimindo os detalhes do livro
-            System.out.println("Livro [titulo= " + meuLivro.titulo + " , autor=" + meuLivro.autor + ", ISBN=" + meuLivro.ISBN +", disponibilidade=" +meuLivro.disponibilidade+ ", criterioDeOrdenacao= "+ meuLivro.criterioDeOrdenacao+ "] \n");
-        }
-
-            // Criando uma nova instância da classe QuickSort
+                livros.insere(meuLivro);
+               
+                System.out.println("Digite o título do livro que deseja remover:");
+                String titulo=leitor.nextLine();
+                livros.retiraPorTitulo(titulo);
+               
+                System.out.println(meuLivro);
            
-            QuickSort quickSort = new QuickSort();
-            // Ordenando a lista de livros usando o QuickSort
-            
-            quickSort.sort(livros, 0, livros.size()-1);
-
-           // Imprima a lista de livros
-            for (int i = 0; i < livros.size(); i++) {
-                Livro livro = livros.get(i);
-                System.out.println("Título: " + livro.getTitulo());
-                System.out.println("Autor: " + livro.getAutor());
-                System.out.println("ISBN: " + livro.getISBN());
-                System.out.println("Disponibilidade: " + livro.getDisponibilidade());
-                System.out.println("Critério de Ordenação: " + livro.getCriterioDeOrdenacao());
-                System.out.println("------------------------");
-            	}
-            
-         
+                System.out.println("Digite o critério de ordenação (titulo ou autor):");
+                String criterio = leitor.nextLine();
+                livros.ordenaBubbleSort(criterio);
+               
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-	
+    // Método para imprimir todos os livros na lista
+    public static void imprimirLivros(Lista livros) {
+        for (int i = 0; i < livros.size(); i++) {
+            Livro livro = livros.get(i);
+            System.out.println("Título: " + livro.getTitulo());
+            System.out.println("Autor: " + livro.getAutor());
+            System.out.println("ISBN: " + livro.getISBN());
+            System.out.println("Disponibilidade: " + livro.getDisponibilidade());
+            System.out.println("Critério de Ordenação: " + livro.getCriterioDeOrdenacao());
+            System.out.println("------------------------");
+        }
+    }
 }
